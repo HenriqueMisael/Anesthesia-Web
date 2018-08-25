fbe791038478dadeac44936f58fd3a86464cca39
HenriqueMisael <henrique.nolear@hotmail.com>
2018-08-25 14:18:02 -0300

    Commit inicial do projeto gradle com spring

---
+++ src/main/java/br/uem/iss/anesthesia/AnesthesiaApplication.java
@@ -0,0 +1,12 @@
+package br.uem.iss.anesthesia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnesthesiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnesthesiaApplication.class, args);
+    }
+}
