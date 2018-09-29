b2fd8f9c2e05a64cd22001c812a1bf3063326cf9
HenriqueMisael <henrique.nolear@hotmail.com>
2018-09-29 12:44:31 -0300

    Implementação do relatório de ficha de paciente

---
+++ src/main/java/br/uem/iss/anesthesia/controller/ReportController.java
@@ -0,0 +1,24 @@
+package br.uem.iss.anesthesia.controller;
+
+import br.uem.iss.anesthesia.model.repository.PatientRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/report")
+public class ReportController {
+
+    private PatientRepository patientRepository;
+
+    public ReportController(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    @GetMapping("/patient-registry/{id}")
+    public ModelAndView patientReport(@PathVariable Long id) {
+        return new ModelAndView("report_patient-registry", "patient", patientRepository.findById(id).get());
+    }
+}
