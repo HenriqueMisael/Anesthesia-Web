7c26b608adbddaf247eb5b3d4c7167072dddce9d
Joao vitor <joaovitormalvestio@gmail.com>
2018-10-01 20:58:07 -0300

    Implementado Medicamentos

---
+++ src/main/java/br/uem/iss/anesthesia/view/MedicineFormView.java
@@ -0,0 +1,12 @@
+package br.uem.iss.anesthesia.view;
+
+import br.uem.iss.anesthesia.model.entity.MedicineModel;
+import org.springframework.web.servlet.ModelAndView;
+
+public class MedicineFormView  extends ModelAndView {
+
+    public MedicineFormView(MedicineModel medicine) {
+        super("layouts/app", "medicine", medicine);
+        this.addObject("conteudo", "form_medicine");
+    }
+}
